21a4dba21b782dbdbbabc1990e1f9a401a50e8a8
Sérgio Filippo <m4n0x.br0@gmail.com>
2025-06-21 16:12:26 -0300

    cadastro

--- estudos/cadastro.sql
+++ estudos/cadastro.sql
@@ -1,9 +1,16 @@
+DROP DATABASE IF EXISTS cadastro;
+CREATE DATABASE cadastro DEFAULT CHARACTER SET utf8mb4 DEFAULT COLLATE utf8mb4_unicode_ci;
 USE cadastro;
+
+DROP TABLE IF EXISTS pessoas;
+
 CREATE TABLE pessoas (
-nome varchar(30),
-idade tinyint,
-sexo char(1),
-peso float,
-altura float,
-nacionalidade varchar(20)
-);
+	id int NOT NULL AUTO_INCREMENT,
+    nome varchar(30) NOT NULL,
+    nascimento date,
+    sexo enum('M','F'),
+    peso decimal(5,2),
+    altura decimal(3,2),
+    nacionalidade varchar(20),
+    primary key (id)
+) DEFAULT CHARSET = utf8mb4;
