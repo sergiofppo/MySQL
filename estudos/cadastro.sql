065a98867595d71e275c9a9bb3fede5ae4095eb6
Sérgio Filippo <m4n0x.br0@gmail.com>
2025-06-22 13:37:35 -0300

    truncate, uptade e delete

--- estudos/cadastro.sql
+++ estudos/cadastro.sql
@@ -1,15 +1,34 @@
-create table if not exists  cursos (
-nome varchar(30) not null unique,
-descricao text,
-carga int unsigned,
-totalaulas int unsigned,
-ano year default '2025'
-) default charset = utf8mb4;
+insert into cursos values
+('1', 'HTML4', 'Curso de HTML5', '40', '37', '2014'),
+('2', 'Algoritmos', 'Lógica de Programação', '20', '15', '2014'), 
+('3', 'Photoshop', 'Dicas de Photoshop CC', '10', '8', '2014'),
+('4', 'PGP', 'Curso de PHP para iniciantes', '40', '20', '2010'),
+('5','Jarva', 'Introdução à Linguagem Java', '10', '29', '2000'),
+('6', 'MySQL', 'Bancos de Dados MySQL', '30', '15', '2016'),
+('7', 'Word', 'Curso completo de Word', '40', '30', '2016'),
+('8', 'Sapateado', 'Danças Rítmicas', '40', '30', '2018'),
+('9', 'Cozinha Árabe', 'Aprenda a fazer Kibe', '40', '30', '2018'),
+('10', 'YouTuber', 'Gerar polêmica e ganhar inscritos', '5', '2', '2018');
 
-desc cursos;
+select * from cursos;
 
-alter table cursos
-add column idcurso int first;
+update cursos
+set nome = 'HTML5'
+where idcurso = '1';
 
-alter table cursos
-add primary key (idcurso);
+update cursos
+set nome = 'Java', carga = '40', ano = '2015'
+where idcurso = '5'
+limit 1;
+
+update cursos
+set nome = 'HTML5'
+where idcurso = '1';
+
+update cursos
+set nome = 'PHP', ano = '2015'
+where idcurso = '4';
+
+delete from cursos
+where idcurso = '10';
+
